41b32f31548dc9c93b3365002c5975a5eebc59ad
Anthonyy232 <Anthonyyla232@gmail.com>
2024-05-02 14:14:11 -0700

    Reverted version number back to 1.0.0 (previously skipped) and bumped version code to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.anthonyla.paperize"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
