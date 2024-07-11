984e34f4251017930d908da388370a50013aafe1
Anthonyy232 <Anthonyyla232@gmail.com>
2024-07-10 18:40:17 -0700

    Added quick-setting tile for changing wallpaper

--- app/src/main/java/com/anthonyla/paperize/feature/wallpaper/wallpaper_alarmmanager/WallpaperBootAndChangeReceiver.kt
+++ app/src/main/java/com/anthonyla/paperize/feature/wallpaper/wallpaper_alarmmanager/WallpaperBootAndChangeReceiver.kt
@@ -2,6 +2,7 @@ package com.anthonyla.paperize.feature.wallpaper.wallpaper_alarmmanager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import com.anthonyla.paperize.core.SettingsConstants
 import com.anthonyla.paperize.data.settings.SettingsDataStore
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,27 +15,26 @@ import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
 /**
- * Receiver for boot and time change events to restart alarm manager
+ * Receiver for boot and time change and wallpaper change events to restart alarm manager
  */
 @AndroidEntryPoint
-class WallpaperBootAndTimeChangeReceiver : BroadcastReceiver() {
+class WallpaperBootAndChangeReceiver : BroadcastReceiver() {
     @Inject lateinit var settingsDataStoreImpl: SettingsDataStore
     override fun onReceive(context: Context, intent: Intent) = goAsync {
         try {
-            if (Intent.ACTION_BOOT_COMPLETED == intent.action || Intent.ACTION_TIME_CHANGED == intent.action || Intent.ACTION_TIMEZONE_CHANGED == intent.action) {
-                val scheduler = WallpaperScheduler(context)
-                val toggleChanger = settingsDataStoreImpl.getBoolean(SettingsConstants.ENABLE_CHANGER) ?: false
-                if (toggleChanger) {
-                    val timeInMinutes1 = settingsDataStoreImpl.getInt(SettingsConstants.HOME_WALLPAPER_CHANGE_INTERVAL) ?: SettingsConstants.WALLPAPER_CHANGE_INTERVAL_DEFAULT
-                    val timeInMinutes2 = settingsDataStoreImpl.getInt(SettingsConstants.LOCK_WALLPAPER_CHANGE_INTERVAL) ?: SettingsConstants.WALLPAPER_CHANGE_INTERVAL_DEFAULT
-                    val scheduleSeparately = settingsDataStoreImpl.getBoolean(SettingsConstants.SCHEDULE_SEPARATELY) ?: false
-                    val alarmItem = WallpaperAlarmItem(
-                        timeInMinutes1 = timeInMinutes1,
-                        timeInMinutes2 = timeInMinutes2,
-                        scheduleSeparately = scheduleSeparately
-                    )
-                    alarmItem.let{scheduler.scheduleWallpaperAlarm(it, null, false, true)}
-                }
+            val scheduler = WallpaperScheduler(context)
+            val toggleChanger = settingsDataStoreImpl.getBoolean(SettingsConstants.ENABLE_CHANGER) ?: false
+            if (toggleChanger) {
+                val timeInMinutes1 = settingsDataStoreImpl.getInt(SettingsConstants.HOME_WALLPAPER_CHANGE_INTERVAL) ?: SettingsConstants.WALLPAPER_CHANGE_INTERVAL_DEFAULT
+                val timeInMinutes2 = settingsDataStoreImpl.getInt(SettingsConstants.LOCK_WALLPAPER_CHANGE_INTERVAL) ?: SettingsConstants.WALLPAPER_CHANGE_INTERVAL_DEFAULT
+                val scheduleSeparately = settingsDataStoreImpl.getBoolean(SettingsConstants.SCHEDULE_SEPARATELY) ?: false
+                val alarmItem = WallpaperAlarmItem(
+                    timeInMinutes1 = timeInMinutes1,
+                    timeInMinutes2 = timeInMinutes2,
+                    scheduleSeparately = scheduleSeparately
+                )
+                val changeImmediate = intent.getBooleanExtra("changeImmediate", false)
+                alarmItem.let{scheduler.scheduleWallpaperAlarm(it, null, changeImmediate, true)}
             }
         } catch (e: Exception) {
             e.printStackTrace()
---
+++ app/src/main/java/com/anthonyla/paperize/feature/wallpaper/wallpaper_tile/ChangeWallpaperTileService.kt
@@ -0,0 +1,38 @@
+package com.anthonyla.paperize.feature.wallpaper.wallpaper_tile
+
+import android.content.Intent
+import android.service.quicksettings.TileService
+import com.anthonyla.paperize.data.settings.SettingsDataStore
+import com.anthonyla.paperize.feature.wallpaper.wallpaper_alarmmanager.WallpaperBootAndChangeReceiver
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+/**
+ * Service for the quick settings tile to change the wallpaper
+ */
+@AndroidEntryPoint
+class ChangeWallpaperTileService: TileService() {
+    @Inject lateinit var settingsDataStoreImpl: SettingsDataStore
+
+    override fun onTileAdded() {
+        super.onTileAdded()
+    }
+
+    override fun onStartListening() {
+        super.onStartListening()
+    }
+
+    override fun onStopListening() {
+        super.onStopListening()
+    }
+
+    override fun onClick() {
+        super.onClick()
+        val intent = Intent(this, WallpaperBootAndChangeReceiver::class.java).putExtra("changeImmediate", true)
+        sendBroadcast(intent)
+    }
+
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+    }
+}
