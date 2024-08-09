9240a89663637f652884c0fcf970977f9a2cf792
Anthonyy232 <Anthonyyla232@gmail.com>
2024-08-08 23:22:34 -0700

    Updated DAO version

--- app/src/main/java/com/anthonyla/paperize/feature/wallpaper/data/data_source/SelectedAlbumDatabase.kt
+++ app/src/main/java/com/anthonyla/paperize/feature/wallpaper/data/data_source/SelectedAlbumDatabase.kt
@@ -12,7 +12,7 @@ import com.anthonyla.paperize.feature.wallpaper.domain.model.Wallpaper
  */
 @Database(
     entities = [Album::class, Wallpaper::class],
-    version = 4
+    version = 5
 )
 @TypeConverters(Converters::class)
 abstract class SelectedAlbumDatabase: RoomDatabase() {
