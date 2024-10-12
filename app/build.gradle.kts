59594878ab79f895479909edc6217840bdf42a08
Anthony La <anthonyyla.dev@gmail.com>
2024-10-12 01:27:29 -0700

    Downgraded NDK version to LTS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.anthonyla.paperize"
         minSdk = 26
         targetSdk = 35
-        versionCode = 33
-        versionName = "2.3.1"
+        versionCode = 34
+        versionName = "2.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
@@ -60,7 +60,7 @@ android {
         generateLocaleConfig = true
     }
 
-    ndkVersion = "28.0.12433566"
+    ndkVersion = "27.1.12297006"
     buildToolsVersion = "35.0.0"
     dependenciesInfo {
         includeInApk = false
