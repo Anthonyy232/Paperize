66c3a4bb2e5cecd83b9d6539ad034d02905668c6
Anthonyy232 <anthonyyla.dev@gmail.com>
2025-09-15 12:47:15 -0700

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.anthonyla.paperize"
         minSdk = 31
         targetSdk = 36
-        versionCode = 41
-        versionName = "3.2.0"
+        versionCode = 42
+        versionName = "3.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
